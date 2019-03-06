ad644be6e4bbb16be0ceebf1881144698c2e114b
boubouhkarim <boubouh.karim@gmail.com>
2019-03-06 23:59:12 +0100

    adding student managment

--- src/main/scala/Department.scala
+++ src/main/scala/Department.scala
@@ -1,21 +1,49 @@
 import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 
 object DProtocol {
+
   case class createCourse(id: Int, name: String)
+
+  case class associateStudent(ar: ActorRef)
+
   case class Response(status: Int)
+
 }
 
 class Department(dName: String) extends Actor {
-  import DProtocol._
+
   import CProtocol._
+  import DProtocol._
+  import UProtocol._
+
+  implicit val timeout = Timeout(10 seconds)
+
   var courses = ListBuffer[ActorRef]()
+  var students = ListBuffer[ActorRef]()
 
   override def receive: Receive = {
     case createCourse(id, name) =>
       val course = context.actorOf(Props(classOf[Course], id, name), "Course" + id + "Actor")
       courses ++= List(course)
       course ! showCourse()
+    case associateStudent(ar) =>
+      val f = context.parent ? studentRegistred(ar)
+      val registered = Await.result(f, timeout.duration).asInstanceOf[Boolean]
+      if (registered) {
+        sender ! "Student " + ar.path + " associated to " + self.path
+        students ++= List(ar)
+      } else {
+        sender ! "Student " + ar.path + " not registered into " + context.parent.path
+      }
+
+
     case _ =>
       println("Class Department")
   }
--- src/main/scala/University.scala
+++ src/main/scala/University.scala
@@ -5,22 +5,30 @@ import scala.util.Random
 
 object UProtocol {
   case class createDepartment(name: String)
-  case class UResponse(actorRef: ActorRef)
+  case class UResponse(ar: ActorRef)
+  case class registerStudent(name: String)
+  case class studentRegistred(actorRef: ActorRef)
 }
 
 class University extends Actor {
   import UProtocol._
   import DProtocol._
   var departments = ListBuffer[ActorRef]()
+  var students = ListBuffer[ActorRef]()
 
   override def receive: Receive = {
     case  createDepartment(name) =>
-      var department = context.actorOf(Props(classOf[Department], name), name+"Actor")
-//      println()
+      val department = context.actorOf(Props(classOf[Department], name), name+"Actor")
 //      context.children.foreach(println)
       department ! createCourse(Random.nextInt(10), name)
       departments ++= List(department)
       sender ! department
+    case registerStudent(name) =>
+      val student = context.actorOf(Props(classOf[Student], name), name+"Actor")
+      students ++= List(student)
+      sender ! student
+    case studentRegistred(ar) =>
+      sender ! students.contains(ar)
     case _ =>
       println("Class University")
   }
